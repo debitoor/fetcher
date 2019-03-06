e06bfd04fcf2f92ebe995fccb59164d9a01b172e
Daniel Hillmann <hiller@live.dk>
2019-03-06 15:57:41 +0100

    changes

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,17 +1,19 @@
-describe('index - fetch', () => {
-	describe('when requesting text source', () => {
-		it('should...', () => {
-
+describe('index', () => {
+	describe('fetch', () => {
+		describe('when requesting text source', () => {
+			it('should...', () => {
+	
+			});
 		});
-	});
-	describe('when requesting json source', () => {
-		it('should...', () => {
-
+		describe('when requesting json source', () => {
+			it('should...', () => {
+	
+			});
 		});
-	});
-	describe('when response does not include json og text header(s)', () => {
-		it('should...', () => {
-
+		describe('when response does not include json og text header(s)', () => {
+			it('should...', () => {
+	
+			});
 		});
 	});
 });
