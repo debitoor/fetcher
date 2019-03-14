e431cf049fd560b8f8a3e64c3a00149d7afb8fe6
Daniel Hillmann <hiller@live.dk>
2019-03-14 12:13:35 +0100

    fix-bug-with-content-type-header (#5)
    * fix bug with content type header
    
    * small mistake fix
    
    * small change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Fetcher {
 			init.body = typeof body === 'object'
 				? JSON.stringify(body)
 				: body;
-			init.headers['Content-Type'] = 'appliation/json';
+			init.headers['Content-Type'] = init.headers['Content-Type'] || 'application/json';
 		}
 
 		const response = await fetch(url, init);
